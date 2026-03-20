d01dd1e68e756afaae39e0afdf8b14a79f906849
Zitong sair <ztzhan1108@gmail.com>
2026-03-20 16:53:30 -0400

    add gif rendering

--- bae/utils/pgo.py
+++ bae/utils/pgo.py
@@ -1,11 +1,67 @@
 import matplotlib.pyplot as plt
-import torch
 import numpy as np
+import torch
+from io import BytesIO
+from PIL import Image
+
+
+def _set_equal_axes(ax):
+    # Keep the 3D plot balanced while letting each frame fit itself.
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    ranges = np.array([x_limits, y_limits, z_limits])
+    span = ranges[:, 1] - ranges[:, 0]
+    max_span = max(span)
+    centers = np.mean(ranges, axis=1)
+    half_span = max_span / 2.0
+    ax.set_xlim3d(centers[0] - half_span, centers[0] + half_span)
+    ax.set_ylim3d(centers[1] - half_span, centers[1] + half_span)
+    ax.set_zlim3d(centers[2] - half_span, centers[2] + half_span)
+
+
+@torch.no_grad()
+def render_frame(points, title='', axlim=None):
+    points = points.detach().cpu().numpy()
+    fig = plt.figure(figsize=(7, 7))
+    ax = plt.axes(projection='3d')
+    ax.plot3D(points[:, 0], points[:, 1], points[:, 2], 'b')
+    plt.title(title)
+    if axlim is not None:
+        ax.set_xlim(axlim[0])
+        ax.set_ylim(axlim[1])
+        ax.set_zlim(axlim[2])
+
+    _set_equal_axes(ax)
+    buffer = BytesIO()
+    fig.savefig(buffer, format='png')
+    image = Image.open(buffer).convert('RGB')
+    image.load()
+    buffer.close()
+    limits = (ax.get_xlim(), ax.get_ylim(), ax.get_zlim())
+    plt.close(fig)
+    return image, limits
+
+
+def save_gif(frames, gifname, duration=250, loop=0):
+    if not frames:
+        raise ValueError('save_gif requires at least one frame')
+
+    first, *rest = frames
+    first.save(
+        gifname,
+        save_all=True,
+        append_images=rest,
+        duration=duration,
+        loop=loop,
+    )
+    print('Saving to', gifname)
 
 @torch.no_grad()
 def plot_and_save(points, pngname, title='', axlim=None):
     points = points.detach().cpu().numpy()
-    plt.figure(figsize=(7, 7))
+    fig = plt.figure(figsize=(7, 7))
     ax = plt.axes(projection='3d')
     ax.plot3D(points[:,0], points[:,1], points[:,2], 'b')
     plt.title(title)
@@ -14,22 +70,9 @@ def plot_and_save(points, pngname, title='', axlim=None):
         ax.set_ylim(axlim[1])
         ax.set_zlim(axlim[2])
 
-
-    # Manually enforce the same range in all directions:
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
-
-    ranges = np.array([x_limits, y_limits, z_limits])
-    span = ranges[:,1] - ranges[:,0]
-    max_span = max(span)
-    centers = np.mean(ranges, axis=1)
-
-    # Update limits so that each axis has the same length:
-    # ax.set_xlim3d(centers[0] - max_span/2, centers[0] + max_span/2)
-    # ax.set_ylim3d(centers[1] - max_span/2, centers[1] + max_span/2)
-    # ax.set_zlim3d(centers[2] - max_span/2, centers[2] + max_span/2)
-
+    _set_equal_axes(ax)
+    limits = (ax.get_xlim(), ax.get_ylim(), ax.get_zlim())
     plt.savefig(pngname)
+    plt.close(fig)
     print('Saving to', pngname)
-    return ax.get_xlim(), ax.get_ylim(), ax.get_zlim()
+    return limits
--- pgo.py
+++ pgo.py
@@ -12,7 +12,7 @@ from bae.utils.ceres_pose import (
 )
 
 from bae.utils.pgo_dataset import G2OPGO
-from bae.utils.pgo import plot_and_save
+from bae.utils.pgo import plot_and_save, render_frame, save_gif
 from pypose.optim.scheduler import StopOnPlateau
 from bae.utils.pysolvers import PCG
 from bae.optim import LM
@@ -134,6 +134,12 @@ if __name__ == '__main__':
                         help="dataset location")
     parser.add_argument("--dataname", type=str, default='parking-garage.g2o', \
                         help="dataset name")  # sphere_bignoise_vertex3, torus3D, grid3D, parking-garage
+    parser.add_argument('--gif', action='store_true', \
+                        help='save an animated GIF of optimization progress')
+    parser.add_argument('--gif-every', type=int, default=1, \
+                        help='capture every N optimization steps in the GIF')
+    parser.add_argument('--gif-duration', type=int, default=250, \
+                        help='GIF frame duration in milliseconds')
     parser.add_argument('--no-vectorize', dest='vectorize', action='store_false', \
                         help="to save memory")
     parser.add_argument('--vectorize', action='store_true', \
@@ -146,6 +152,8 @@ if __name__ == '__main__':
     parser.set_defaults(vectorize=True)
     args = parser.parse_args(); print(args)
     os.makedirs(os.path.join(args.save), exist_ok=True)
+    if args.gif_every < 1:
+        raise ValueError('--gif-every must be >= 1')
 
     data = G2OPGO(args.dataroot, args.dataname, device=args.device, download=True)
     if isinstance(data.nodes, pp.LieTensor):
@@ -168,9 +176,6 @@ if __name__ == '__main__':
         input = {'edges': edges, 'poses': poses, 'infos': infos}
         graph = PoseGraph(data.nodes).to(args.device)
     solver = PCG(tol=1e-5)
-    # solver = ppos.Cholesky()
-    # strategy = ppost.TrustRegion(radius=1e4, min=1e-32, max=1e16)
-    # strategy = pp.optim.strategy.TrustRegion(up=2.0, down=0.5**4)
     strategy = pp.optim.strategy.Adaptive()
 
     optimizer = LM(graph, solver=solver, strategy=strategy, min=1e-10, reject=30)
@@ -181,10 +186,14 @@ if __name__ == '__main__':
     else:
         nodes_current = graph.nodes
 
-    pngname = os.path.join(args.save, args.dataname+'.png')
-    axlim = plot_and_save(pp.SE3(nodes_current).translation(), pngname, args.dataname)
-    axlim = None
-    ### the 1st implementation: for customization and easy to extend
+    sample_prefix = os.path.join(args.save, os.path.splitext(args.dataname)[0])
+    plot_and_save(pp.SE3(nodes_current).translation(), sample_prefix + '.png', args.dataname)
+
+    gif_frames = []
+    if args.gif:
+        frame, _ = render_frame(pp.SE3(nodes_current).translation(), args.dataname)
+        gif_frames.append(frame)
+
     if args.device == 'cuda':
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
@@ -195,8 +204,15 @@ if __name__ == '__main__':
         loss = optimizer.step(input=input, weight=infos)
         scheduler.step(loss)
 
-        name = os.path.join(args.save, args.dataname + '_' + str(scheduler.steps))
+        name = os.path.join(args.save, os.path.splitext(args.dataname)[0] + '_' + str(scheduler.steps))
         title = 'PGO at the %d step(s) with loss %7f'%(scheduler.steps, loss.item())
+        if args.gif and ((i + 1) % args.gif_every == 0 or i == args.steps - 1):
+            if args.gauge_fix:
+                nodes_current = graph.nodes_all(input['node_fixed'])
+            else:
+                nodes_current = graph.nodes
+            frame, _ = render_frame(pp.SE3(nodes_current).translation(), title)
+            gif_frames.append(frame)
     if args.device == 'cuda':
         torch.cuda.synchronize()
         end.record()
@@ -209,9 +225,11 @@ if __name__ == '__main__':
         nodes_current = graph.nodes_all(input['node_fixed'])
     else:
         nodes_current = graph.nodes
-    plot_and_save(pp.SE3(nodes_current).translation(), name+'.png', title, axlim=axlim)
+    plot_and_save(pp.SE3(nodes_current).translation(), name+'.png', title)
     torch.save(graph.state_dict(), name+'.pt')
     write_ceres_txt(nodes_current, name+'.txt')
+    if args.gif:
+        save_gif(gif_frames, sample_prefix + '.gif', duration=args.gif_duration)
 
     ### The 2nd implementation: equivalent to the 1st one, but more compact
     # scheduler.optimize(input=(edges, poses, infos))
